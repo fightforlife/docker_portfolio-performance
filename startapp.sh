bf0f5688c9f9193cf3310aa8d1fcf38e6f1d85cc
Frederik <5511687+fightforlife@users.noreply.github.com>
2021-01-03 13:12:02 +0100

    Create startapp.sh

---
+++ startapp.sh
@@ -0,0 +1,5 @@
+#startapp.sh
+## Taken end editred from: https://forum.portfolio-performance.info/t/portfolio-performance-in-docker/10062
+#!/bin/sh
+cd /opt/portfolio/workspace
+exec /opt/portfolio/PortfolioPerformance
